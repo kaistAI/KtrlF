9aa2a9c3f4a05adfbe54d73b0e20235d3809e0ce
Haebin Shin <sunsal0704@gmail.com>
2023-11-16 19:43:02 +0900

    tentative url push

--- ktrlf_plugin/src/scripts/utils.js
+++ ktrlf_plugin/src/scripts/utils.js
@@ -4,5 +4,5 @@ export async function getCurrentTabId() {
     return tab.id;
 }
 
-export const KTRLF_URL = "";
-export const LINKPREVIEW_URL = "";
+export const KTRLF_URL = "https://proved-quest-korean-involving.trycloudflare.com";
+export const LINKPREVIEW_URL = "https://api.linkpreview.net/?key=a7db9ddfd68a1b848e6c02adfabd3f32&q=";
